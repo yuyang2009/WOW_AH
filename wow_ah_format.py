2ef7b1af3fc742101f72f81c4762db5365c91c24
yuyang <yuyang20091881@163.com>
2016-10-05 23:26:44 +0800

    todo:字符串对齐；程序打包

--- wow_ah_format.py
+++ wow_ah_format.py
@@ -4,7 +4,6 @@ import os
 import sys
 import json
 import time
-import datetime
 import winsound
 import pandas as pd
 from collections import namedtuple
@@ -13,14 +12,14 @@ auc_url = "http://auction-api-cn.worldofwarcraft.com/auction-data/d9f2775ea64400
 snap_url = "https://wowtoken.info/snapshot.json"
 report_list = [u"物品名称", u"最低价格", u"数量", u"我的价格"]
 item = namedtuple('item', ['id', 'name', 'low', 'high'])
-row_format = u"{:<20}" * (len(report_list))
+row_format = u"{:<15}" * (len(report_list))
+cutoff_line = u"* "*30
 bn_id = u"道亦有盗"
 
-app_path = sys.path[0]
+app_path = os.path.abspath(sys.path[0])
 file_name = "item_list.txt"
 file_path = os.path.join(app_path, file_name)
-alter_flag = False
-report_flag = True
+
 
 def get_auc():
     try:
@@ -61,26 +60,35 @@ def get_items():
     f_items.close()
     return items
 
-def report(item, df):
-    name = item.name.decode("utf8")
-    df.loc[:, 'unit_price'] = df.loc[:, 'buyout'] / df.loc[:, 'quantity'] / 10000
-    low = df['unit_price'].min()
-    quantity = df.loc[df['unit_price'] == low]['quantity'].sum()
-    my_price = df.loc[df['owner'] == bn_id]['unit_price'].mean()
-    alter_low = item.low
-    alter_high = item.high
-    if alter_low.strip() and low<int(alter_low) or alter_high.strip() and low>int(alter_high):
-        print '\033[32m', row_format.format(name, low, quantity, my_price), '\033[0m'
-        global alter_flag
-        alter_flag = True
-    else:
-        print row_format.format(name, low, quantity, my_price)
-
+def report(items, auc_df):
+    print u"当前时间：%s" %time.ctime()
+    print u"时光徽章: %s\t" %snap_cn_buy, u"更新时间: %s " %time_updated
+    print cutoff_line
+    print row_format.format(*report_list)
+    alter_flag = False
+    for item in items:
+        if item.id:
+            df = auc_df.loc[auc_df['item'] == int(item.id)]
+            name = item.name.decode("utf8")
+            df.loc[:, 'unit_price'] = df.loc[:, 'buyout'] / df.loc[:, 'quantity'] / 10000
+            low = df['unit_price'].min()
+            quantity = df.loc[df['unit_price'] == low]['quantity'].sum()
+            my_price = df.loc[df['owner'] == bn_id]['unit_price'].mean()
+            alter_low = item.low
+            alter_high = item.high
+            if alter_low.strip() and low<int(alter_low) or alter_high.strip() and low>int(alter_high):
+                print '\033[32m', row_format.format(name, low, quantity, my_price), '\033[0m'
+                alter_flag = True
+            else:
+                print row_format.format(name, low, quantity, my_price)
+    print cutoff_line, "\n"
+    if alter_flag:
+        winsound.Beep(300,2000)
 
 if __name__ == '__main__':
-    import sys
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
+    #import sys
+    #reload(sys)
+    #sys.setdefaultencoding('utf-8')
     items = get_items()
     if len(items) == 0:
         print u"监视列表为空！"
@@ -88,36 +96,25 @@ if __name__ == '__main__':
 
     snap_updated = ''
     auc_lenth = 0
+    print '\033[32m', u"查询中……\n", '\033[0m'
     while True:
         alter_flag = False
-        report_flag = True
         snap_cn_buy, time_updated = get_snapshot()
-        time_now = datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
         auc_df = get_auc()
         #Comparisons to singletons like None should always be done with is or is not, never the equality operators.
         #用 is 或者 is not 来判断对象是否为空
-        if not snap_updated or time_updated is not None and snap_updated!=time_updated:
-            print u"当前时间: %s" %time_now
+        if not snap_updated and auc_lenth==0:
+            report(items, auc_df)
+        elif auc_df is not None and auc_lenth!=len(auc_df):
+            report(items, auc_df)
+        elif time_updated is not None and snap_updated!=time_updated:
+            print u"当前时间：%s" %time.ctime()
             print '\033[32m', u"时光徽章: %s\t" %snap_cn_buy, u"更新时间: %s " %time_updated, '\033[0m'
-
+            print cutoff_line, "\n"
         #循环中存在auc_df为 NoneType 错误，可能为resp.read()读取失败，也可能是urlopen失败
-        elif auc_lenth==0 or auc_df is not None and auc_lenth!=len(auc_df):
-            print u"当前时间: %s" %time_now
-            print u"时光徽章: %s\t" %snap_cn_buy, u"更新时间: %s " %time_updated
         else:
-            report_flag = False
             continue
         auc_lenth = len(auc_df)
         snap_updated = time_updated
 
-        print "* " *40
-        report_df = pd.DataFrame(columns = report_list)
-        print row_format.format(*report_list)
-        for item in items:
-            if item.id:
-                df = auc_df.loc[auc_df['item'] == int(item.id)]
-                report(item, df)
-        print "* " *40, "\n"
-        if alter_flag:
-            winsound.Beep(300,2000)
         time.sleep(20)

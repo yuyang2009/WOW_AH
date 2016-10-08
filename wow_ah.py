4751403a8051598b628c63fef1fa9bc486d7713d
yuyang2009 <yuyang20091881@163.com>
2016-10-08 16:44:12 +0800

    bug fix

--- wow_ah.py
+++ wow_ah.py
@@ -1,12 +1,14 @@
 #-*- coding: UTF-8 -*-
 import urllib
-from os import path
+import contextlib
 import sys
 import json
 import time
-import winsound
+#import winsound
+import logging
 import unicodedata
 import pandas as pd
+from os import path
 from collections import namedtuple
 
 #auc_url = "http://auction-api-cn.worldofwarcraft.com/auction-data/d9f2775ea6440077e5de6749fc37d81e/auctions.json"
@@ -17,7 +19,7 @@ row_format = u"{:<{}}" * (len(report_list))
 cutoff_line = u"* "*30
 ft_color = ['37', '36']
 width = 15
-
+timeout = 10
 #RealmName = u"影牙要塞"
 #bn_id = u"盗亦有道"
 
@@ -31,11 +33,13 @@ player_info_name = "Player_info.txt"
 player_info_path = path.join(app_path, player_info_name)
 
 def get_Player_info():
+    logging.info("get_Player_info start")
     with open(player_info_path) as player_info_file:
         player_info_line = player_info_file.read()
         if player_info_line:
             player_info = player_info_line.decode('utf8').split(',')
             if len(player_info)==2:
+                logging.info("get_Player_info end")
                 return player_info[0].strip(), player_info[1].strip()
             else:
                 print '\033[31m', u"%s: 玩家信息描述文件格式错误，请确认分隔符为英文逗号。\n" %time.ctime(), '\033[0m'
@@ -45,10 +49,12 @@ def get_Player_info():
             exit()
 
 def get_RealmAPI(RealmName):
+    logging.info("RealmName start")
     with open(realmurls_path) as realm_file:
         for line in realm_file:
             API_dic = line.decode("utf8").split()
             if API_dic[0] == RealmName:
+                logging.info("RealmName end")
                 return API_dic[1]
             else:
                 pass
@@ -56,6 +62,7 @@ def get_RealmAPI(RealmName):
         exit()
 
 def get_items():
+    logging.info("get_items start")
     items = []
     with open(file_path, 'r') as f_items:
         for line in f_items:
@@ -65,6 +72,7 @@ def get_items():
                 items.append(temp)
     f_items.close()
     if items is not None and len(items) != 0:
+        logging.info("get_items end")
         return items
     else:
         print '\033[31m', u"%s: 监视列表为空！\n" %time.ctime(), '\033[0m'
@@ -81,33 +89,74 @@ def insert_width(list, width):
     return ret_list
 
 def get_auc():
-    try:
-        resp = urllib.urlopen(auc_url)
-        #resp_json = json.load(resp)
-        resp_json = json.loads(resp.read())
-        auc_json = resp_json.get('auctions')
-        auc_df = pd.DataFrame(auc_json)
-        return auc_df
+    logging.info("get_auc start")
+    with contextlib.closing(urllib.urlopen(auc_url)) as resp:
+        logging.info(resp.code)
+        try:
+            resp_json = json.loads(resp.read())
+            #resp_json = json.load(resp)
+            auc_json = resp_json.get('auctions')
+            auc_df = pd.DataFrame(auc_json)
+            logging.info("get_auc end")
+            return auc_df
+        except:
+            #print '\033[31m', u"%s: 拍卖行数据读取失败\n" %time.ctime(), '\033[0m'
+            logging.info('urllib error')
+            return None
 
-    except:
-        # print u"当前时间：%s" %time.ctime()
-        # print '\033[31m', u"拍卖行API异常", '\033[0m'
-        # print cutoff_line, "\n"
-        print '\033[31m', u"%s: 拍卖行API异常\n" %time.ctime(), '\033[0m'
-        return None
+    # try:
+    #     resp = urllib.urlopen(auc_url)
+    #     #resp_json = json.load(resp)
+    #     resp_json = json.loads(resp.read())
+    #     auc_json = resp_json.get('auctions')
+    #     auc_df = pd.DataFrame(auc_json)
+    #     logging.info("get_auc end")
+    #     resp.close()
+    #     return auc_df
+
+    # except:
+    #     # print u"当前时间：%s" %time.ctime()
+    #     # print '\033[31m', u"拍卖行API异常", '\033[0m'
+    #     # print cutoff_line, "\n"
+    #     print '\033[31m', u"%s: 拍卖行API异常\n" %time.ctime(), '\033[0m'
+    #     logging.info("get_auc error")
+    #     resp.close()
+    #     return None
 
 def get_snapshot():
-    try:
-        resp = urllib.urlopen(snap_url)
-        #resp_json = json.load(resp)
-        resp_json = json.loads(resp.read())
-        snap_cn_json = resp_json.get('CN').get('formatted')
-        return snap_cn_json['buy'], snap_cn_json['updated']
-    except:
-        print '\033[31m', u"%s: 时光徽章API异常\n" %time.ctime(), '\033[0m'
-        return None, None
+    logging.info("get_snapshot start")
+    with contextlib.closing(urllib.urlopen(snap_url)) as resp:
+        logging.info(resp.code)
+        try:
+            resp_json = json.loads(resp.read())
+            snap_cn_json = resp_json.get('CN').get('formatted')
+            logging.info("get_snapshot end")
+            return snap_cn_json['buy'], snap_cn_json['updated']
+        except:
+            #print '\033[31m', u"%s: 时光徽章读取失败\n" %time.ctime(), '\033[0m'
+            logging.info('urllib error')
+            return None, None
+    # try:
+    #     resp = urllib.urlopen(snap_url)
+    #     #resp_json = json.load(resp)
+    #     resp_json = json.loads(resp.read())
+    #     snap_cn_json = resp_json.get('CN').get('formatted')
+    #     logging.info("get_snapshot end")
+    #     #return snap_cn_json['buy'], snap_cn_json['get_snapshot updated']
+    #     #JSON api 修改
+    #     if snap_cn_json['buy'] is None or snap_cn_json['updated'] is None:
+    #         print '\033[31m', u"%s: 时光徽章API格式修改\n" %time.ctime(), '\033[0m'
+    #         logging.info("snapshot format error")
+    #     resp.close()
+    #     return snap_cn_json['buy'], snap_cn_json['updated']
+    # except:
+    #     resp.close()
+    #     print '\033[31m', u"%s: 时光徽章API异常\n" %time.ctime(), '\033[0m'
+    #     logging.info("get_snapshot error")
+    #     return None, None
 
 def report(items, auc_df):
+    logging.info("report start")
     print u"当前时间：%s" %time.ctime()
     print u"服务器名称：%s\t玩家ID：%s" %(RealmName, bn_id)
     print u"时光徽章: %s\t" %snap_cn_buy, u"更新时间: %s " %time_updated
@@ -138,10 +187,14 @@ def report(items, auc_df):
             color_flag = (color_flag+1)%2
     print cutoff_line, "\n"
     if alter_flag:
-        winsound.Beep(300,2000)
+        #winsound.Beep(300,2000)
+        pass
+    logging.info("report end")
 
 if __name__ == '__main__':
     #global RealmName, bn_id
+    logging.basicConfig(filename='wow_ah.log', filemode='w', filemaxBytes=5 * 1024, level=logging.DEBUG, format='%(asctime)s - (%(lineno)d) - %(funcName)s - %(message)s')
+
     RealmName, bn_id = get_Player_info()
     auc_url = get_RealmAPI(RealmName)
     items = get_items()
@@ -151,16 +204,9 @@ if __name__ == '__main__':
     auc_lenth = 0
     print '\033[32m', u"查询中……\n", '\033[0m'
     while True:
-        #start = time.clock()
-
+        logging.info("查询开始")
         snap_cn_buy, time_updated = get_snapshot()
         auc_df = get_auc()
-
-        now = time.ctime()
-        # if auc_df is not None:
-        #     print u'%s: 拍卖行物品数量为 %d \n' %(now, len(auc_df))
-        # else:
-        #     print u'拍卖行API异常\n'
 
         #Comparisons to singletons like None should always be done with is or is not, never the equality operators.
         #用 is 或者 is not 来判断对象是否为空
@@ -178,12 +224,12 @@ if __name__ == '__main__':
             print cutoff_line, "\n"
             snap_updated = time_updated
         else:
-            time.sleep(5)
-            continue
+            pass
 
         #auc_lenth = len(auc_df)
         #snap_updated = time_updated
 
         # end = time.clock()
         # print u'%s: 运行时间了 %d 秒\n' %(now, (end-start))
+        logging.info("查询结束")
         time.sleep(5)
